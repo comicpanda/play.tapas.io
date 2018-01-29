e76548c5240028df7c228312e9dbdb0fd08d5f81
YoonKim <isyoon@tapasmedia.co>
2018-01-28 18:49:57 -0800

    Add firebase auth/firestore

---
+++ public/javascripts/app.js
@@ -0,0 +1,20 @@
+const config = {
+    apiKey: 'AIzaSyDEbzhEvdQWpQ4UhIdQZyi1nWLytADxfDM',
+    authDomain: 'tapasmedia-co-api-tapastic-android.firebaseapp.com',
+    databaseURL: 'https://tapasmedia-co-api-tapastic-android.firebaseio.com',
+    projectId: 'tapasmedia.co:api-tapastic-android'
+  };
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log(user.uid)
+  } else {
+    new firebaseui.auth.AuthUI(firebase.auth()).start('#firebaseui-auth-container', {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+    });
+  }
+});
